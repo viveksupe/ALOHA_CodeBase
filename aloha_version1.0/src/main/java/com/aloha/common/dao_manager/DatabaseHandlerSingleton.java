0bda878d53795b219e1a0c145b29035d7d35da67
Milind Gokhale <milindhg@gmail.com>
2015-04-11 15:08:10 -0400

    changes in status and

--- aloha_version1.0/src/main/java/com/aloha/common/dao_manager/DatabaseHandlerSingleton.java
+++ aloha_version1.0/src/main/java/com/aloha/common/dao_manager/DatabaseHandlerSingleton.java
@@ -28,10 +28,10 @@ public class DatabaseHandlerSingleton {
 			stmt = con.createStatement();
 
 		} catch (SQLException ex) {
-			System.out.println("cannot query the database");
+			System.out.println("cannot query the database: " + ex.getMessage());
 		} catch (ClassNotFoundException ex) {
-			System.out.println("cannot connect to MySQL database");
-			ex.printStackTrace();
+			System.out.println("cannot connect to MySQL database" + ex.getMessage());
+			//ex.printStackTrace();
 		}
 	}
 
@@ -58,29 +58,3 @@ public class DatabaseHandlerSingleton {
 	}
 
 }
-
-/*
- * import java.sql.*;
- * 
- * 
- * public class DatabaseHandler { private static String driverName =
- * "com.mysql.jdbc.Driver"; private static String url =
- * "jdbc:mysql://localhost:3306/"; private static String dbName = "testdb";
- * private static String userName = "root"; private static String password =
- * "root"; private static Connection connection = null; public void configure()
- * { driverName = "com.mysql.jdbc.Driver"; url = "jdbc:mysql://localhost:3306/";
- * dbName = "testdb"; userName = "root"; password = "root"; } public static
- * Connection getConnectionObject(){ while(connection==null) { try {
- * Class.forName(driverName); connection =
- * DriverManager.getConnection(url+dbName,userName,password); }
- * catch(ClassNotFoundException ex) {
- * System.out.println("ERROR: Unable to Connect to Database."); }catch
- * (SQLException ex) {
- * System.out.println("ERROR: Unable to Connect to Database."); } } try {
- * connection.setAutoCommit(true);
- * connection.setHoldability(ResultSet.CLOSE_CURSORS_AT_COMMIT); } catch
- * (SQLException ex) { ex.printStackTrace(); } return connection; }
- * 
- * 
- * }
- */
