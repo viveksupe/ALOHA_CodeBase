c25fb92f3f3da726573e75e0134c6516b18912f2
renukadeshmukh <renud1988@gmail.com>
2015-04-17 22:28:35 -0400

    changes to comment dal to accept username

--- aloha_version1.0/src/main/java/com/aloha/common/dao_manager/dal/CommentDal.java
+++ aloha_version1.0/src/main/java/com/aloha/common/dao_manager/dal/CommentDal.java
@@ -24,12 +24,14 @@ public class CommentDal {
 	private String INSERT_COMM;
 	private String UPDATE_COMM;
 	private String DELETE_COMM;
+	private String CUSTOM_SELECT;
 	
 	public CommentDal(){
 		SELECT = "SELECT comment.comment_id, comment.comment_content,comment.timestamp, comment.post_id,comment.user_id FROM comment";
 		INSERT_COMM = "INSERT INTO comment ( comment_content, user_id, post_id) VALUES ( ?, ?, ?);";
 		UPDATE_COMM = "UPDATE comment SET comment_content = ?, user_id = ?, post_id = ?,timestamp=current_timestamp WHERE comment_id = ?;";
 		DELETE_COMM = "DELETE FROM comment ";
+		CUSTOM_SELECT = "SELECT c.comment_id, c.comment_content,c.timestamp, c.post_id,c.user_id,u.fname,u.lname FROM comment c	inner join user u on c.user_id = u.user_id where c.post_id = ?";
 		con = DatabaseHandlerSingleton.getDBConnection();
 	}
 	
@@ -101,6 +103,45 @@ public class CommentDal {
 		}
 
 	}
+
+	public ArrayList<Comment> getCustomCommentForPost(int post_id) throws SQLException{
+		String getPostComms = CUSTOM_SELECT;
+		PreparedStatement ps = null;
+		ResultSet rSet = null;
+		try {
+			con = DatabaseHandlerSingleton.getDBConnection();
+			ps = con.prepareStatement(getPostComms);
+			ps.setInt(1, post_id);
+			rSet = ps.executeQuery();
+			ArrayList<Comment> comms = new ArrayList<Comment>();
+			while (rSet.next()) {
+				Comment comm = new Comment(
+						rSet.getInt("comment_id"),
+						rSet.getString("comment_content"), 
+						rSet.getTimestamp("timestamp"),
+						rSet.getInt("post_id"), 
+						rSet.getInt("user_id"));
+				String fname = rSet.getString("fname");
+				String lname = rSet.getString("lname");
+				comm.setUserName(fname + " " + lname);
+
+				comms.add(comm);
+			}
+			return comms;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if (rSet != null)
+				rSet.close();
+			if (ps != null)
+				ps.close();
+			con.close();
+		}
+
+	}
+
 
 	public int deleteComment(int comment_id) throws SQLException{
 		String deleteStmt = DELETE_COMM + " WHERE comment_id = ?";
