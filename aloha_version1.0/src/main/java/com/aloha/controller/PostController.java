f530ef286c4d29fe68b8977c5ab8cb810f181ff5
renukadeshmukh <renud1988@gmail.com>
2015-04-18 23:44:29 -0400

    methods added for like and dislike in controller

--- aloha_version1.0/src/main/java/com/aloha/controller/PostController.java
+++ aloha_version1.0/src/main/java/com/aloha/controller/PostController.java
@@ -19,6 +19,8 @@ import com.aloha.common.entities.Comment;
 import com.aloha.common.entities.Post;
 import com.aloha.common.entities.user.User;
 import com.aloha.common.model.CommentUI;
+import com.aloha.common.model.DislikeUI;
+import com.aloha.common.model.LikeUI;
 import com.aloha.common.model.PostUI;
 
 
@@ -119,4 +121,29 @@ public class PostController {
 		return result;
 	}
 	
+	@RequestMapping(value="like", method=RequestMethod.POST)
+	public @ResponseBody int likePost(@RequestParam("postId") int postId,@RequestParam("userId") int userId){
+		LikeUI lui = new LikeUI(userId,postId);
+		int result = -1;
+		try {
+			result = lui.like(lui);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	@RequestMapping(value="dislike", method=RequestMethod.POST)
+	public @ResponseBody int dislikePost(@RequestParam("postId") int postId,@RequestParam("userId") int userId){
+		DislikeUI lui = new DislikeUI(userId,postId);
+		int result = -1;
+		try {
+			result = lui.dislike(lui);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
 }
