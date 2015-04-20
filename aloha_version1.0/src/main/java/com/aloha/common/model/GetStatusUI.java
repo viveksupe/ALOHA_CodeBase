16bb51f1f312a650b1ad684ddafb9a43c479c03c
renukadeshmukh <renud1988@gmail.com>
2015-04-20 18:12:06 -0400

    updating comments, posts and likes and dislikes to use proxy

---
+++ aloha_version1.0/src/main/java/com/aloha/common/model/GetStatusUI.java
@@ -0,0 +1,13 @@
+package com.aloha.common.model;
+
+public class GetStatusUI extends ResponseUI {
+	boolean actionStatus;
+
+	public boolean isActionStatus() {
+		return actionStatus;
+	}
+
+	public void setActionStatus(boolean actionStatus) {
+		this.actionStatus = actionStatus;
+	}
+}
--- aloha_version1.0/src/main/java/com/aloha/controller/IPostController.java
+++ aloha_version1.0/src/main/java/com/aloha/controller/IPostController.java
@@ -1,41 +1,29 @@
 package com.aloha.controller;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Locale;
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.aloha.common.dao_manager.dal.UserDal;
-import com.aloha.common.entities.user.User;
-import com.aloha.common.model.CommentUI;
-import com.aloha.common.model.DislikeUI;
+import com.aloha.common.model.GetCommentUI;
+import com.aloha.common.model.GetLikeStatusUI;
+import com.aloha.common.model.GetPostUI;
 import com.aloha.common.model.GetPostsUI;
-import com.aloha.common.model.LikeUI;
-import com.aloha.common.model.PostUI;
+import com.aloha.common.model.GetStatusUI;
 
 public interface IPostController {
 
 	
 	public GetPostsUI getAllPosts(String searchKey, Model model, HttpSession session);
 	
+	public GetPostUI addPost(String post, HttpSession session);
 	
+	public GetCommentUI addComment(String comm, int postId, HttpSession session) ;
 	
-	public PostUI addPost(String post, HttpSession session);
+	public GetStatusUI deletePost( int postId, HttpSession session);
 	
-	public CommentUI addComment(String comm, int userId, int postId, HttpSession session) ;
+	public GetStatusUI deleteComment(int commId, HttpSession session);
 	
-	public boolean deletePost( int postId, HttpSession session);
-	
-	public boolean deleteComment(int commId, HttpSession session);
-	
-	public int likePost( int postId, int userId, int likeType, HttpSession session );
+	public GetLikeStatusUI likePost( int postId, int likeType, HttpSession session );
 
-	public int dislikePost( int postId, int userId,  int dislikeType, HttpSession session);
+	public GetLikeStatusUI dislikePost( int postId, int dislikeType, HttpSession session);
 }
