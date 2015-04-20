a5d3c922fa146ee354012145d27c5b115d0c02cf
renukadeshmukh <renud1988@gmail.com>
2015-04-20 13:16:59 -0400

    proxy for postcontroller

---
+++ aloha_version1.0/src/main/java/com/aloha/controller/ProxyPostController.java
@@ -0,0 +1,91 @@
+package com.aloha.controller;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Locale;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.aloha.common.model.CommentUI;
+import com.aloha.common.model.PostUI;
+import com.aloha.common.model.ResponseUI;
+import com.aloha.common.model.UserUI;
+
+@Controller
+public class ProxyPostController implements IPostController {
+
+	private PostController postController;
+
+	@RequestMapping("post")
+	public String setup(Locale locale, Model model) throws SQLException{
+		
+		return "postContainer";
+	}
+	
+	@RequestMapping(value = "post/getAll", method=RequestMethod.POST)
+	@Override
+	public @ResponseBody ArrayList<PostUI> getAllPosts(@RequestParam("searchKey") String searchKey, Model model,
+			HttpSession session) {
+		// TODO Auto-generated method stub
+		boolean isValid = validateSessionUser(session);
+		if(isValid){
+		postController = new PostController();
+		return postController.getAllPosts(searchKey, model,session);
+		}
+		
+	}
+
+
+	@Override
+	public PostUI addPost(String post, HttpSession session) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CommentUI addComment(String comm, int userId, int postId,
+			HttpSession session) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deletePost(int postId, HttpSession session) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteComment(int commId, HttpSession session) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int likePost(int postId, int userId, int likeType,
+			HttpSession session) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int dislikePost(int postId, int userId, int dislikeType,
+			HttpSession session) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public boolean validateSessionUser(HttpSession session){
+		if(null==session.getAttribute("sessionUser")){
+			return false;
+		}
+		return true;
+	}
+}
